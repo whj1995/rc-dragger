6a8afc0e5a99156597e0ce11765251239bcb2bee
whj1995 <wu38607@live.com>
2019-03-03 11:38:32 +0800

    .

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './demo/index.tsx',
+  entry: './example/index.ts',
   mode: 'development',
   devtool: 'source-map',
   devServer: {
